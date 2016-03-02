cb958e23649248b71eb4804f605066fd6b68205f
cmiller <caitlinhmiller@gmail.com>
2016-03-02 21:30:24 +1300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+__author__ = 'marvin'
+
+class CobrasTofu:
+
+    def __init__(self):
+        pass
+
+    def replace_names(self, obfuscatee):
+        return obfuscatee
+
+    # TODO
+    def add_junk(self):
+        pass
+
+    # TODO think of more obfuscations
+    # TODO monetize monetize monetize
+
+f = open('test_input')
+print CobrasTofu().replace_names(f.read())
