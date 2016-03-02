886e2e6b073d507358c7c9d88beb1bbb04934f2e
cmiller <caitlinhmiller@gmail.com>
2016-03-02 22:21:55 +1300

    replace_names: First pass at finding & replacing variable names

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 __author__ = 'marvin'
 
+import re
+
 class CobrasTofu:
+
+    variable_declaration = re.compile("^[\s]*[a-zA-Z_][a-zA-Z0-9_]*[\s]*=[\s]*")
 
     def __init__(self):
         pass
 
     def replace_names(self, obfuscatee):
-        return obfuscatee
+        varname_set = set()
+        obfuscated = ""
+        for line in obfuscatee.readlines():
+            if self.variable_declaration.match(line):
+                #print "line: " + line
+                varname_set.add(re.compile("^[\s]*" + line[:line.index('=')].strip() + "[\s]+"))
+                subd = re.sub(self.variable_declaration, "VAR=", line)
+                #print subd
+            for varname in varname_set:
+                if re.match(varname, line):
+                    line = re.sub(varname, self._generate_varname(), line) + "\n"
+            obfuscated += line
+
+        return obfuscated
+
+    def _generate_varname(self):
+        return "varvarvarvar"
 
     # TODO
     def add_junk(self):
@@ -16,4 +36,4 @@ class CobrasTofu:
     # TODO monetize monetize monetize
 
 f = open('test_input')
-print CobrasTofu().replace_names(f.read())
+print CobrasTofu().replace_names(f)
